dadd0593c89786e36a680212b26ae8de042e4e0a
shivnath-p <127609928+shivnath-p@users.noreply.github.com>
2023-03-13 18:28:28 +0530

    Update test4.c

--- Week1/test4.c
+++ Week1/test4.c
@@ -2,23 +2,20 @@
 #include<stdio.h>
 void main()
 {
-    float x,y,a,s,m,d;
-    printf("1 addition\n");
-    printf("2 subtraction\n");
-    printf("3 multiplication\n");
-    printf("4 division\n");
+    float x,y;
     printf("enter two numbers");
     scanf("%f%f",&x,&y);
-    a=x+y;
-    s=x-y;
-    m=x*y;
-    d=x/y;
     switch(1)
     {
-        case 1:printf("the addition of two numbers are %f\n",a);
-        case 2:printf("the subtraction of two numbers are %f\n",s);
-        case 3:printf("the multiplication of two numbers are %f\n",s);
-        case 4:printf("the division of two numbers are %f\n",s);
+        case 1:printf("the addition of two numbers are %f\n",x+y);
+            break;
+        case 2:printf("the subtraction of two numbers are %f\n",x-y);
+            break;
+        case 3:printf("the multiplication of two numbers are %f\n",x*y);
+            break;
+        case 4:printf("the division of two numbers are %f\n",x/y);
+            break;
+        default:printf("error");
     }
 
 }
